bdecde5d6a15aa5f01868c92a5258dcadb507d42
Syed Haider <syedshahbaz@live.co.uk>
2021-12-12 23:45:10 +0100

    Configured Dependency Injection

--- SqlBulkInsertTool/Program.cs
+++ SqlBulkInsertTool/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using SqlBulkInsertTool.Process;
 
 namespace SqlBulkInsertTool
 {
     class Program
     {
+        public static readonly IServiceProvider Container = new ContainerBuilder().Build();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Container.GetServices<IProcess>().ToList().ForEach(p => p.Process());
         }
     }
 }
